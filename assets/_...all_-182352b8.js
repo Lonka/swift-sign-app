68cc65def9412e36809319382fe019ec7cdf967a
lonka.liu <lonka.liu@gmail.com>
2023-04-18 01:18:34 +0800

    1

---
+++ assets/_...all_-182352b8.js
@@ -0,0 +1,1 @@
+import{d as e,a as o,o as a,c as s,t as c,f as r,v as n}from"./index-7d73666b.js";const f=e({__name:"[...all]",setup(l){const{t}=o();return(p,u)=>(a(),s("div",null,c(r(t)("not-found")),1))}});typeof n=="function"&&n(f);export{f as default};
